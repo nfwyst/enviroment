2be81efc8592f9496c45f9df1bf8249342abd195
nfwyst <nfwyst@gmail.com>
2022-09-25 19:13:30 +0800

    modify colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,24 @@
+vim.g.NeoSolarized_italics = 1 -- 0 or 1
+vim.g.NeoSolarized_visibility = 'normal' -- low, normal, high
+vim.g.NeoSolarized_diffmode = 'normal' -- low, normal, high
+vim.g.NeoSolarized_termtrans = 1 -- 0(default) or 1 -> Transparency
+vim.g.NeoSolarized_lineNr = 0 -- 0 or 1 (default) -> To Show backgroung in LineNr
+
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme NeoSolarized
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
+
+augroup BgHighlight
+  autocmd!
+  highlight FloatBorder guibg=NONE ctermbg=NONE  " Removes the border of float menu (LSP and Autocompletion uses it)
+  highlight link NormalFloat Normal
+  highlight NormalFloat ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+  highlight Pmenu ctermbg=NONE guibg=NONE
+  highlight ColorColumn ctermbg=238
+augroup END
 ]]
+

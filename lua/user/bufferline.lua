cebfdf1e0fc92d47175d0e3e0a629a1bfe8b97dd
nfwyst <nfwyst@gmail.com>
2022-09-30 19:42:41 +0800

    dont show icon in bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -14,11 +14,9 @@ bufferline.setup {
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
     indicator = { style = "icon", icon = "▎" },
-    buffer_close_icon = "",
     -- buffer_close_icon = '',
-    modified_icon = "●",
-    close_icon = "",
     -- close_icon = '',
+    modified_icon = "●",
     left_trunc_marker = "",
     right_trunc_marker = "",
     --- name_formatter can be used to change the buffer's label in the bufferline.
@@ -57,8 +55,8 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators

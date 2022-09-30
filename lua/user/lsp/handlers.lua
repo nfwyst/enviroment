7de6333ccada02e1a4ee883c1b8f4f2f8c784a86
nfwyst <nfwyst@gmail.com>
2022-09-30 23:35:17 +0800

    disable lsp that support formatting & setup null-ls & fix format typo & remove useless Format command

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -68,14 +68,23 @@ local function lsp_keymaps(bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format{async=true}' ]])
+end
+
+local function contains(list, value)
+  for _, v in ipairs(list) do
+    if v == value then
+      return true
+    end
+  end
+  return false
 end
 
 M.on_attach = function(client, bufnr)
 	-- vim.notify(client.name .. " starting...")
-	-- TODO: refactor this into a method that checks if string in list
-	if client.name == "tsserver" then
+  local disable_formatting_lsp = { "tsserver", "ccls", "sumneko_lua" }
+	if contains(disable_formatting_lsp, client.name) then
 		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)

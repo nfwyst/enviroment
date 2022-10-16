d2446f826850e3d7d31ccb15bd732b73512131ad
nfwyst <nfwyst@gmail.com>
2022-10-16 15:53:26 +0800

    remove open all folds on startup & format comment.lua

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,8 +1,8 @@
 vim.cmd [[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
   augroup end
@@ -21,7 +21,7 @@ vim.cmd [[
 
   augroup _auto_resize
     autocmd!
-    autocmd VimResized * tabdo wincmd = 
+    autocmd VimResized * tabdo wincmd =
   augroup end
 
   augroup _alpha
@@ -31,14 +31,14 @@ vim.cmd [[
 
   augroup _tsfold
     autocmd!
-    autocmd BufWinEnter * normal zx | zR
+    autocmd BufWinEnter * normal zx
   augroup end
 
   augroup _outlines
     autocmd!
     autocmd FileType Outline set nospell
   augroup end
-  
+
   if executable("node")
     augroup _coderun
       autocmd bufread,bufnewfile *.js noremap <leader>r :% w !node<enter>
--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -1,40 +1,39 @@
 local status_ok, comment = pcall(require, "Comment")
 if not status_ok then
-  return
+	return
 end
 
-comment.setup {
-  pre_hook = function(ctx)
-    local U = require "Comment.utils"
-    local location = nil
-    local type = '__default' or '__multiline'
-    local cursor_location = require('ts_context_commentstring.utils').get_cursor_location()
-    local visual_start_location = require('ts_context_commentstring.utils').get_visual_start_location()
+comment.setup({
+	pre_hook = function(ctx)
+		local U = require("Comment.utils")
+		local location = nil
+		local type = "__default" or "__multiline"
+		local cursor_location = require("ts_context_commentstring.utils").get_cursor_location()
+		local visual_start_location = require("ts_context_commentstring.utils").get_visual_start_location()
 
-    -- Only calculate commentstring for tsx filetypes
-    if vim.bo.filetype == 'typescriptreact' then
-        if ctx.ctype == U.ctype.blockwise then
-            location = cursor_location
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = visual_start_location
-        end
+		-- Only calculate commentstring for tsx filetypes
+		if vim.bo.filetype == "typescriptreact" then
+			if ctx.ctype == U.ctype.blockwise then
+				location = cursor_location
+			elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+				location = visual_start_location
+			end
 
-        return require('ts_context_commentstring.internal').calculate_commentstring({
-            key = ctx.ctype == U.ctype.linewise and type,
-            location = location,
-        })
-    end
+			return require("ts_context_commentstring.internal").calculate_commentstring({
+				key = ctx.ctype == U.ctype.linewise and type,
+				location = location,
+			})
+		end
 
-    if ctx.ctype == U.ctype.block then
-      location = cursor_location
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = visual_start_location
-    end
+		if ctx.ctype == U.ctype.block then
+			location = cursor_location
+		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+			location = visual_start_location
+		end
 
-    return require("ts_context_commentstring.internal").calculate_commentstring({
-      key = ctx.ctype == U.ctype.line and type,
-      location = location,
-    })
-  end,
-}
-
+		return require("ts_context_commentstring.internal").calculate_commentstring({
+			key = ctx.ctype == U.ctype.line and type,
+			location = location,
+		})
+	end,
+})

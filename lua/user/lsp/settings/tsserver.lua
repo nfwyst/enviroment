9d6a6d3c163a29f643efd9a3088b53903904d6ce
nfwyst <nfwyst@gmail.com>
2022-11-04 08:15:04 +0800

    filter some lsp diagnostics codes

--- lua/user/lsp/settings/tsserver.lua
+++ lua/user/lsp/settings/tsserver.lua
@@ -1,12 +1,17 @@
 local status_ok, util = pcall(require, "lspconfig/util")
 if not status_ok then
-  return {}
+	return {}
 end
 
 return {
-  root_dir = function (pattern)
-    local cwd  = vim.loop.cwd();
-    local root = util.root_pattern("package.json", "tsconfig.json", ".git")(pattern);
-    return root or cwd;
-  end;
+	root_dir = function(pattern)
+		local cwd = vim.loop.cwd()
+		local root = util.root_pattern("package.json", "tsconfig.json", ".git")(pattern)
+		return root or cwd
+	end,
+	settings = {
+		diagnostics = {
+			ignoredCodes = { 7016, 80001, 80006, 80007 },
+		},
+	},
 }

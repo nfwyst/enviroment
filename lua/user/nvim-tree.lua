588895cc06acb0a6b4d5ca8a6ba20fe75ce49292
nfwyst <nfwyst@gmail.com>
2022-11-30 19:31:33 +0800

    chore(severity): set severity min to error

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -55,6 +55,9 @@ nvim_tree.setup({
 	diagnostics = {
 		enable = true,
 		show_on_dirs = true,
+		severity = {
+			min = vim.diagnostic.severity.ERROR,
+		},
 		icons = {
 			hint = "",
 			info = "",

372a000b9e76df06096a326db0f8424542aa1bd5
nfwyst <nfwyst@gmail.com>
2022-11-30 13:32:27 +0800

    fix(tree): disable update root and git ignore

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,76 +1,80 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-  return
+	return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup {
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  renderer = {
-    root_folder_modifier = ":t",
-    icons = {
-      show = {
-        folder_arrow = false,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-    indent_width = 2,
-    indent_markers = {
-      enable = false,
-      inline_arrows = true,
-    },
-  },
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  view = {
-    adaptive_size = true,
-    width = 30,
-    side = "right",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-  },
-}
+nvim_tree.setup({
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		update_root = false,
+	},
+	renderer = {
+		root_folder_modifier = ":t",
+		icons = {
+			show = {
+				folder_arrow = false,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				folder = {
+					arrow_open = "",
+					arrow_closed = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "",
+					staged = "",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+		indent_width = 2,
+		indent_markers = {
+			enable = false,
+			inline_arrows = true,
+		},
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	view = {
+		adaptive_size = true,
+		width = 30,
+		side = "right",
+		mappings = {
+			list = {
+				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+				{ key = "h", cb = tree_cb("close_node") },
+				{ key = "v", cb = tree_cb("vsplit") },
+			},
+		},
+	},
+	git = {
+		ignore = false,
+	},
+})
